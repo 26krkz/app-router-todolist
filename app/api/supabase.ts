30fac973b09f72af3859ce03a1c4268aca94eb40
github 26krkz <26krkz@gmail.com>
2024-02-05 16:57:25 +0900

    chore: fetch apiをサーバーで実行できるように修正

--- app/api/supabase.ts
+++ app/api/supabase.ts
@@ -3,10 +3,10 @@ import { Database } from "@/database.types";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error("環境変数を設定してください");
 }
 
-const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 export default supabase;
--- app/ui/Form/index.tsx
+++ app/ui/Form/index.tsx
@@ -4,20 +4,13 @@ import styles from "./styles.module.css";
 import { fetchTodos } from "@/app/api";
 
 export const Form = () => {
-  const handleClickAddBtn = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    const todos = await fetchTodos();
-    console.log(todos);
-  };
   return (
-    <form className={styles.form}>
+    <form className={styles.form} action={fetchTodos}>
       <div className={styles.inputWrapper}>
         <input placeholder="TODOを追加する" className={styles.input}></input>
         <input placeholder="メモを書く" className={styles.input}></input>
       </div>
-      <Button className={styles.addBtn} onClick={(e) => handleClickAddBtn(e)}>
-        add
-      </Button>
+      <Button className={styles.addBtn}>add</Button>
     </form>
   );
 };
