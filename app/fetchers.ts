a81ee58449e11df445b9908e9114d863eb5ee7b5
github 26krkz <26krkz@gmail.com>
2024-02-26 17:53:44 +0900

    chore: setTimeoutで遅延が長すぎてbuildが落ちてるようなので調整。

--- app/fetchers.ts
+++ app/fetchers.ts
@@ -17,7 +17,7 @@ export const fetchTodos = () => {
       } catch (err) {
         reject(err);
       }
-    }, 2000);
+    }, 1000);
   });
 };
 
@@ -36,6 +36,6 @@ export const fetchCompletedTodos = () => {
       } catch (err) {
         reject(err);
       }
-    }, 2000);
+    }, 1000);
   });
 };
